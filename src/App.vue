05405f3f98065afd7626551a786702ea27885752
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-17 12:35:34 +0300

    feat: add component SongsPlayer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import SongsList from "./components/SongsList.vue";
-import type { Song } from "./types";
-import { getSongs } from "./firebase/songs";
+import SongsList from "@/components/SongsList.vue";
+import SongsPlayer from "@/components/SongsPlayer.vue";
+import type { Song } from "@/types";
+import { getSongs } from "@/firebase/songs";
 
 const songs = ref<Song[]>([]);
 onMounted(() => getSongs(songs));
 </script>
 
 <template>
-  <main><SongsList :songs="songs" /></main>
+  <main>
+    <SongsPlayer />
+    <SongsList :songs="songs" />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/SongsPlayer.vue
@@ -0,0 +1,15 @@
+<script setup lant="ts"></script>
+
+<template>
+  <v-card title="Player" class="player mx-auto pa-2 mb-4" max-width="300">
+    <v-card-text>
+      <v-icon icon="mdi-play"> </v-icon>
+      Song title
+    </v-card-text>
+    <v-card-actions>
+      <v-btn>Add to favorites</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped></style>
